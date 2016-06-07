282b2c2dd7b5bf9ccf16c73f42cebc56b2643e89
bojan.gajic <bgajic@gmail.com>
2016-06-07 09:20:56 +0200

    added properties file

--- CustomerDistance/src/main/java/io/intracom/util/MatchCustomer.java
+++ CustomerDistance/src/main/java/io/intracom/util/MatchCustomer.java
@@ -2,6 +2,7 @@ package io.intracom.util;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.FileSystems;
@@ -10,6 +11,7 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Properties;
 import java.util.logging.Logger;
 
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -17,14 +19,46 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 
 public class MatchCustomer {
-	private static final String INPUT = "E:\\gistfile1.txt";
-	private static final String CHARSET = "UTF-8";
-	private static final double DUBLIN_LAT = 53.3381985;
-	private static final double DUBLIN_LNG = -6.2592576;
+
+	private static String INPUT = "C:\\gistfile1.txt";
+	private static String CHARSET = "UTF-8";
+	private static double DUBLIN_LAT = 53.3381985;
+	private static double DUBLIN_LNG = -6.2592576;
 
 	private final static Logger LOGGER = Logger.getLogger(MatchCustomer.class.getName());
 
+	static {
+		String filename = "props.properties";
+		Properties props = new Properties();
+		InputStream input = MatchCustomer.class.getClassLoader().getResourceAsStream(filename);
+
+		try {
+			if (input == null) {
+				throw new IOException("Sorry, unable to find " + filename);
+			}
+			props.load(input);
+
+			INPUT = props.getProperty("file.location");
+			CHARSET = props.getProperty("charset");
+			DUBLIN_LAT = Double.parseDouble(props.getProperty("dublin.lat"));
+			DUBLIN_LNG = Double.parseDouble(props.getProperty("dublin.lon"));
+
+		} catch (IOException e) {
+			LOGGER.warning("unable to find properties file!");
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					LOGGER.warning("unable to close properties file!");
+				}
+			}
+		}
+	}
+
 	public static void main(String[] args) {
+		MatchCustomer mc = new MatchCustomer();
+
 		String path = INPUT;
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		System.out.print("Enter path to JSON file: ");
@@ -37,7 +71,6 @@ public class MatchCustomer {
 			LOGGER.severe("Problem with console input!");
 		}
 
-		MatchCustomer mc = new MatchCustomer();
 		try {
 			mc.match(path);
 		} catch (IOException e) {
